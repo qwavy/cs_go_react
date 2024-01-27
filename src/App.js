401f196520ff81cade22052571f139991fcc2819
unknown <nursultanr585@gmail.com>
2024-01-28 02:55:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./pages/Header/Header"
+import MainPage from './pages/MainPage/MainPage'
+
+const App = () => {
+  return(
+    <div>
+      <MainPage/>
+    </div>
+  )
+}
+
+export default App
